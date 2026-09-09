0df5dfc7203c7e9670d092268eb5024b148d8d17
MR LWAZI.Z <mrscorpion204@gmail.com>
2026-09-09 22:57:29 +0200

    Add files via upload

---
+++ settings.js
@@ -0,0 +1,16 @@
+const settings = {
+  packname: '𝚁𝚒𝚙𝚙𝚎𝚛-𝚡𝚍',
+  author: '‎',
+  botName: "𝚁𝙸𝙿𝙿𝙴𝚁-𝚇𝙳",
+  botOwner: '𝚁𝙸𝙿𝙿𝙴𝚁-𝚇𝙳', // Your name
+  ownerNumber: '27736324314', //Set your number here without + symbol, just add country code & number without any space
+  giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
+  commandMode: "public",
+  maxStoreMessages: 20, 
+  storeWriteInterval: 10000,
+  description: "This is a bot for managing group commands and automating tasks.",
+  version: "3.0.7",
+  updateZipUrl: "",
+};
+
+module.exports = settings;
