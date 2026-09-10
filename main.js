c52e8dcf517b790d0293b0d76e7f11f6ef60671c
MR LWAZI.Z <mrscorpion204@gmail.com>
2026-09-10 22:02:18 +0200

    Update main.js

--- main.js
+++ main.js
@@ -146,8 +146,8 @@ const soraCommand = require('./commands/sora');
 // Global settings
 global.packname = settings.packname;
 global.author = settings.author;
-global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp3A";
-global.ytch = "Mr Unique Hacker";
+global.channelLink = "https://whatsapp.com/channel/0029Va90zAnIHphOuO8Msp";
+global.ytch = "Mr Lwazi";
 
 // Add this near the top of main.js with other global configurations
 const channelInfo = {
@@ -155,8 +155,8 @@ const channelInfo = {
         forwardingScore: 1,
         isForwarded: true,
         forwardedNewsletterMessageInfo: {
-            newsletterJid: '120363161513685998@newsletter',
-            newsletterName: 'KnightBot MD',
+            newsletterJid: '',
+            newsletterName: 'TECH XX',
             serverMessageId: -1
         }
     }
